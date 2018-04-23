d604ff410c02d25184467e62332c878231f809cc
Ilya Makovoz <imakovoz93@gmail.com>
2018-04-23 16:15:58 -0400

    add music

--- main.js
+++ main.js
@@ -2,11 +2,33 @@ const Game = require('./lib/game.js');
 const createCard = require('./lib/createCard.js');
 const chooseHazard = require('./lib/chooseHazard.js');
 const setup = require('./lib/setup.js');
+const helpModal = require('./lib/helpModal.js');
 
 document.addEventListener('DOMContentLoaded', function(event) {
-  const game = new Game();
-  $('#game').data('game', game);
-  setup.setup(game);
-  const hazards = game.drawHazards();
-  chooseHazard.chooseHazard(hazards, game);
+  document.querySelector('audio').muted = true;
+  helpModal.helpModal();
+  const mainGame = document.querySelector('#main-game');
+  let startGame = document.createElement('div');
+  startGame.id = 'startGame';
+  startGame.textContent = 'Start Game?';
+  mainGame.innerHTML += startGame.outerHTML;
+  document.querySelector('#help').addEventListener('click', e => {
+    helpModal.helpModal();
+  });
+  document.querySelector('#music').addEventListener('click', e => {
+    if (document.querySelector('audio').muted) {
+      document.querySelector('audio').muted = false;
+      document.querySelector('#music').className = 'musicOn';
+    } else {
+      document.querySelector('audio').muted = true;
+      document.querySelector('#music').className = 'musicOff';
+    }
+  });
+  document.querySelector('#startGame').addEventListener('click', e => {
+    const game = new Game();
+    $('#game').data('game', game);
+    setup.setup(game);
+    const hazards = game.drawHazards();
+    chooseHazard.chooseHazard(hazards, game);
+  });
 });
